a9bfd07511eb8db021f60abe5613c535351e3d63
DianaTeresa <20120238@student.hcmus.edu.vn>
2023-11-07 21:10:33 +0700

    Change network to lenet-5

--- mini-dnn-cpp-master/demo.cc
+++ mini-dnn-cpp-master/demo.cc
@@ -27,23 +27,25 @@
 
 int main() {
   // data
-  MNIST dataset("../data/mnist/");
+  MNIST dataset("../data/FashionMnist/");
   dataset.read();
   int n_train = dataset.train_data.cols();
   int dim_in = dataset.train_data.rows();
-  std::cout << "mnist train number: " << n_train << std::endl;
-  std::cout << "mnist test number: " << dataset.test_labels.cols() << std::endl;
+  std::cout << "Fashion MNIST train number: " << n_train << std::endl;
+  std::cout << "Fashion MNIST test number: " << dataset.test_labels.cols() << std::endl;
   // dnn
   Network dnn;
-  Layer* conv1 = new Conv(1, 28, 28, 4, 5, 5, 2, 2, 2);
-  Layer* pool1 = new MaxPooling(4, 14, 14, 2, 2, 2);
-  Layer* conv2 = new Conv(4, 7, 7, 16, 5, 5, 1, 2, 2);
-  Layer* pool2 = new MaxPooling(16, 7, 7, 2, 2, 2);
-  Layer* fc3 = new FullyConnected(pool2->output_dim(), 32);
-  Layer* fc4 = new FullyConnected(32, 10);
+  Layer* conv1 = new Conv(1, 6, 1, 28, 28, 6, 5, 5, 1, 2, 2, 28, 28);
+  Layer* pool1 = new MaxPooling(6, 28, 28, 6, 2, 2, 1, 6, 14, 14, 6);
+  Layer* conv2 = new Conv(6, 16, 6, 14, 14, 16, 5, 5, 1, 2, 2, 10, 10);
+  Layer* pool2 = new MaxPooling(16, 10, 10, 16, 2, 2, 1, 16, 5, 5, 16);
+  Layer* fc3 = new FullyConnected(pool2->output_dim(), 120);
+  Layer* fc4 = new FullyConnected(120, 84);
+  Layer* fc5 = new FullyConnected(84, 10);
   Layer* relu1 = new ReLU;
   Layer* relu2 = new ReLU;
   Layer* relu3 = new ReLU;
+  Layer* relu4 = new ReLU;
   Layer* softmax = new Softmax;
   dnn.add_layer(conv1);
   dnn.add_layer(relu1);
@@ -54,6 +56,8 @@ int main() {
   dnn.add_layer(fc3);
   dnn.add_layer(relu3);
   dnn.add_layer(fc4);
+  dnn.add_layer(relu4);
+  dnn.add_layer(fc5);
   dnn.add_layer(softmax);
   // loss
   Loss* loss = new CrossEntropy;
@@ -95,3 +99,4 @@ int main() {
   }
   return 0;
 }
+
